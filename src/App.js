2301a38b80b7949dec84e5ce064ced8abee16405
zhimin8 <hajimin2006@naver.com>
2020-06-13 18:43:03 +0900

    2020.06.13 18:40 React 선행학습

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import TOC from './components/TOC';
+import Content from './components/Content';
+import Subject from './components/Subject';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      mode : 'welcome',
+      subject:{title:"WEB", sub:"World Wide Web"},
+      welcome:{title:"welcome", desc:"hello, React!"},
+      contents:[
+        {id:1, title:"HTML", desc:"HTML 파일에 대한 설명"},
+        {id:2, title:"CSS", desc:"css 파일에 대한 설명"},
+        {id:3, title:"Java Script", desc:"js 파일에 대한 설명"}
+      ]
+    }
+  }
+  render(){
+    var _title, _desc = null;
+    if(this.state.mode == 'welcome'){
+      _title = this.state.welcome.title;
+      _desc = this.state.welcome.desc;
+    } else if(this.state.mode == 'read'){
+      _title = this.state.contents[0].title;
+      _desc = this.state.contents[0].desc;
+    
+    }
+
+    return (
+      <div className="App">
+        <Subject title={this.state.subject.title} sub={this.state.subject.sub}></Subject>
+        <TOC data={this.state.contents}></TOC>
+        <Content title={_title} sub={_desc}></Content>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './App'; // 실제로 구현하게 되는 것
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root') // 이 쿼리를 기반으로 해서, public-index의 root를 이용해서 만든다,
+  
 );
 
 // If you want your app to work offline and load faster, you can change

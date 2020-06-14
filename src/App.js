7d38fd9fe9842fba43ca8c0b324c4d813c7b8c0c
zhimin8 <hajimin2006@naver.com>
2020-06-15 03:58:50 +0900

    2020.06.15 React 선행학습

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
 import TOC from './components/TOC';
-import Content from './components/Content';
+import ReadContent from './components/ReadContent';
+import CreateContent from './components/CreateContent';
+import UpdateContent from './components/UpdateContent';
 import Subject from './components/Subject';
-import './App.css';
+import Control from './components/Control';
 
 class App extends Component{
   constructor(props){
     super(props);
+    this.max_content_id = 3;
     this.state = {
       mode : 'welcome',
+      selected_content_id:2,
       subject:{title:"WEB", sub:"World Wide Web"},
       welcome:{title:"welcome", desc:"hello, React!"},
       contents:[
@@ -18,25 +22,110 @@ class App extends Component{
       ]
     }
   }
-  render(){
-    var _title, _desc = null;
+  getReadContent(){
+    var i = 0;
+    while( i < this.state.contents.length){
+      //현재 순번에 해당하는 contents
+      var data = this.state.contents[i];
+      if(data.id === this.state.selected_content_id){
+        return data;
+        break;
+      }
+      i = i + 1;
+    }
+  }
+  getContent(){
+    var _title, _desc, _article = null;
     if(this.state.mode == 'welcome'){
       _title = this.state.welcome.title;
       _desc = this.state.welcome.desc;
-    } else if(this.state.mode == 'read'){
-      _title = this.state.contents[0].title;
-      _desc = this.state.contents[0].desc;
+      _article = <ReadContent title={_title} sub={_desc}></ReadContent>;
     
-    }
+    } else if(this.state.mode == 'read'){
+        var _content = this.getReadContent();
+        _article = <ReadContent title={_content.title} desc={_content.desc}></ReadContent>;
 
+    } else if(this.state.mode == 'create'){
+      _article = <CreateContent onSubmit={function(_title, _desc){
+        this.max_content_id = this.max_content_id +1;
+        var _contents = this.state.contents.concat(
+          {id: this.max_content_id, title : _title, desc : _desc}
+        );
+        this.setState({
+          contents: _contents
+        });
+      }.bind(this)}></CreateContent>
+
+    } else if(this.state.mode == 'update'){
+      _content = this.getReadContent();
+      _article = <UpdateContent data={_content} 
+        onSubmit={function(_id, _title, _desc){
+        var _contents = Array.from(this.state.contents);
+        var i = 0 ;
+        while( i < _contents.length){
+          if(_contents[i].id === _id){
+            _contents[i] = {id:_id, title:_title, desc:_desc};
+            break;
+          }
+          i++;
+        }
+        this.setState({
+          contents: _contents
+        });
+      }.bind(this)}></UpdateContent>
+    }
+    return _article;
+  }
+  
+  render(){
     return (
       <div className="App">
-        <Subject title={this.state.subject.title} sub={this.state.subject.sub}></Subject>
-        <TOC data={this.state.contents}></TOC>
-        <Content title={_title} sub={_desc}></Content>
+       <Subject 
+        title={this.state.subject.title} 
+        sub={this.state.subject.sub}
+        onChangePage={function(){
+          this.setState({
+            mode:'welcome'
+          })
+        }.bind(this)}>
+        </Subject>
+        <TOC 
+        onChangePage={function(id){
+          this.setState({
+            mode:'read',
+            selected_content_id : Number(id)
+          });
+        }.bind(this)}
+        data={this.state.contents}></TOC>
+        {/* // 함수 내에서는 mode라는 매개변수로 전달 됨. */}
+        <Control onChangeMode={function(_mode){
+          if(_mode === 'delete'){
+            if(window.confirm('really?')){
+              var _contents = Array.from(this.state.contents);
+              var i = 0;
+              while( i < _contents.length){
+                if(_contents[i].id == this.state.selected_content_id){
+                  _contents.splice(i,1);
+                  break;
+                }
+                i = i + 1;
+              }
+              this.setState({
+                mode:'welcome',
+                contents:_contents
+              });
+              alert('삭제완료!');
+            }
+          } else {
+            this.setState({
+              mode : _mode
+            });
+          }
+        }.bind(this)}></Control>
+        {this.getContent()}
       </div>
     );
-  }
+      }
 }
 
 export default App;
